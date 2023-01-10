3396ffa43272911fa566d91ec45496864d7eca1f
IdoTzurTal <idorexxl@gmail.com>
2023-01-10 19:40:18 +0200

    aboutpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import HomePage from "./HomePage";
-import { Route, Routes } from "react-router-dom";
 import Login from "./LogIn"
 import './App.css';
 import About from './Pages/About'
@@ -12,7 +11,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/Home" element={<HomePage />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
         <Route path='/About' element={<About />}></Route>
         <Route path='/Contact' element={<Contact />}></Route>
       </Routes>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,27 @@
-
+import { NavLink } from "react-router-dom"
+import About from "./About"
+import { Tab } from "@mui/material"
+import LogIn from "../LogIn"
+import { Box } from "@mui/system"
+import HomePage from "../HomePage"
+import Tabs from "@mui/material/Tabs";
 
 function Contact() {
     return (
+        <div>
+        <div>
+        <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <Tabs centered>
+          <NavLink to='/Home' element={<HomePage/>} > <Tab label="cyber pro" /></NavLink>
+          <NavLink> <Tab label="meeting rooms" /></NavLink>
+          <NavLink> <Tab label="lecture rooms" /></NavLink>
+          <NavLink> <Tab label="classrooms" /></NavLink>
+          <NavLink to='/About' element={<About/>} > <Tab label="about" /></NavLink>
+          <NavLink to='/Contact' element={<Contact/>} > <Tab label="contact" /></NavLink>
+          <NavLink to='/Login' element={<LogIn/>}> <Tab label="log in" /></NavLink>
+        </Tabs>
+      </Box>
+      </div>
         <div id="content">
             <div>
                 <div class="firstrow">
@@ -23,6 +43,7 @@ function Contact() {
 
             </div>
 
+        </div>
         </div>
     )
 }
