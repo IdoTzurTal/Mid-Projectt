10465772488b15a7ce4628eea770e9a2248c97fd
Liorbenhamo <liorbenhamo159@gmail.com>
2023-01-10 14:39:44 +0200

    moved from main

---
+++ src/Class.jsx
@@ -0,0 +1,47 @@
+import "./class.css";
+import * as React from "react";
+
+function Class() {
+  return (
+    <div>
+      <div id="fleximgdescription">
+        <div id="classinfo">
+          <div>
+            <h2>classnumber</h2>
+          </div>
+          <div>
+            <p>descripsion</p>
+          </div>
+        </div>
+        <div id="classimg">
+          <img
+            src="https://www.cta.org/wp-content/uploads/2020/03/empty-classroom.jpg"
+            alt="classroom img"
+          />
+        </div>
+      </div>
+      <div></div>
+      <div>info</div>
+      <div>
+        <h3>Cancellation rules and company policy</h3>
+        <ul>
+          <li>
+            Cancellations made 7 days or more in advance of the event date, will
+            receive a 100% refund.{" "}
+          </li>
+          <li>
+            Cancellations made within 48 hours to the event will incur a 30%
+            fee.
+          </li>
+          <li>Cancellations made within 3 - 6 days will incur a 20% fee.</li>
+          <li>
+            I understand that I am holding a spot so reservations for this event
+            are nonrefundable. If I am unable to attend I understand that I can
+            transfer to a friend
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Class;
