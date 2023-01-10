bed200383b164743431e2aee626be1981295a76c
IdoTzurTal <idorexxl@gmail.com>
2023-01-10 10:03:30 +0200

    About and main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import HomePage from "./HomePage";
-import { Route, Routes } from "react-router-dom";
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+      <Route path='/About' element={<About/>}></Route>
+      <Route path='/Contact' element={<Contact/>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/About.jsx
@@ -0,0 +1,24 @@
+
+
+function About() {
+    return (
+        <div class="prodetails">
+            <span id="gibson">
+
+                Officeworks is the world’s biggest office lease and booking platform. You might know us from our world-class
+                facilities, our supportive community, or our awesome Officeworks utilities.
+
+                We collaborate with the world's finest companies to bring the forefront of easy office booking right to the palm of your hand.
+                Officeworks gives your business the commercial office space solutions it needs – minus the headaches of upfront capital and lengthy leases. That’s why more than 50% of Fortune 100 companies are WeWork members.
+
+                On top of that, imagine accomodating in some of the best equipped facilities in the world, while also gaining access to said companies' office perks and features. That's Officeworks.
+            </span>
+            <span id="fender">
+                Create a workplace that meets all your business requirements with your pick of Grade A buildings, branding, security, IT and more.
+            </span>
+        </div>
+
+    )
+}
+
+export default About
